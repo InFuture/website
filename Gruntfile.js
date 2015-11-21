a3ed092e1d9dfb6c36723ad9e2e38e97245abef0
liamrahav <liorogamerdvd@gmail.com>
2015-11-21 09:26:23 -0500

    Added grunt workflow - some commands need work (copy)

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        sass: {
+            compile: {
+                options: {
+                    style: 'compressed',
+                    sourcemap: 'none',
+                },
+                files: {
+                    'dist/ss/main.css': 'src/ss/main.scss'
+                }
+            }
+        },
+
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html': 'src/index.html'
+                }
+            }
+        },
+
+        uglify: {
+            minify: {
+                files: {
+                    'dist/js/jquery.min.js': 'src/js/jquery.min.js',
+                    'dist/js/jquery.plusanchor.min.js' : 'src/js/jquery.plusanchor.min.js',
+                    'dist/js/jquery.easing.min.js' : 'src/js/jquery.easing.min.js',
+                    'dist/js/script.js' : ['src/js/faq.js','src/js/nav.js']
+                }
+            }
+        },
+
+        copy: {
+            copy: {
+                options: {
+                    timestamp: true,
+                },
+                main: {
+                    nonull: true,
+                    files: [
+                        { expand: true, src: ['src/images/**'], dest: 'dist/images/' },
+                    ]
+                }
+            }
+        },
+
+        watch: {
+            style: {
+                files: ['src/ss/**/*.scss', '*.html', '*.js', "src/images/**"],
+                tasks: ['sass', 'htmlmin', 'uglify', 'copy']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['sass:compile', 'uglify', 'htmlmin', 'copy', 'watch']);
+}
