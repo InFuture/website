6a233036bad3c1b880246363f5d0008477b64836
liamrahav <liorogamerdvd@gmail.com>
2015-11-22 23:08:05 -0500

    up to you @jayhxmo

--- src/js/sub.js
+++ src/js/sub.js
@@ -4,6 +4,28 @@ $("#submitButton").click(function() {
 		$("#submitForm").serialize(),
 		function(data) {
 			data = $.parseJSON(data);
-			console.log(data["message"]);
+			if (data["success"] == 1) {
+				// Good
+				$(".intro > .container").append("<p class=\"response good\">" + data["message"] + "</p>")
+			} else {
+				// Bad
+				$(".intro > .container").append("<p class=\"response bad\">" + data["message"] + "</p>")
+			}
+		}, 'json');
+});
+
+$("#submitButton2").click(function() {
+	$.post(
+		"infuture.io/subscribe", 
+		$("#submitForm2").serialize(),
+		function(data) {
+			data = $.parseJSON(data);
+			if (data["success"] == 1) {
+				// Good
+				$(".intro > .container").append("<p class=\"response good\">" + data["message"] + "</p>")
+			} else {
+				// Bad
+				$(".intro > .container").append("<p class=\"response bad\">" + data["message"] + "</p>")
+			}
 		}, 'json');
 });
