ca257450d37a3e7c06889e64464cc48c6197df14
liamrahav <liorogamerdvd@gmail.com>
2015-11-26 15:30:03 -0500

    fixes

--- src/js/sub.js
+++ src/js/sub.js
@@ -1,47 +1,32 @@
-$("#submitButton").click(function () {
-	if (validateEmail($("#submitForm").val())) {
-		$.post(
-			"infuture.io/subscribe",
-			$("#submitForm").serialize(),
-			function (data) {
-				data = $.parseJSON(data);
-				if (data["success"] == 1) {
-					// Good
-					$(".intro > .container").append("<p class=\"response good\">" + data["message"] + "</p>")
-				} else {
-					// Bad
-					$(".intro > .container").append("<p class=\"response bad\">" + data["message"] + "</p>")
-				}
-			}, 'json');
-	}
+$("#submitForn").submit(function () {
+	$.post(
+		"infuture.io/subscribe",
+		$("#submitForm").serialize(),
+		function (data) {
+			data = $.parseJSON(data);
+			if (data["success"] == 1) {
+				// Good
+				$(".intro > .container").append("<p class=\"response good\">" + data["message"] + "</p>")
+			} else {
+				// Bad
+				$(".intro > .container").append("<p class=\"response bad\">" + data["message"] + "</p>")
+			}
+		}, 'json');
 });
 
-$("#submitButton2").click(function () {
-	if (validateEmail($("#submitForm2").val())) {
-		console.log($("#submitForm2").serialize());
-		$.post(
-			"infuture.io/subscribe",
-			// $(#submitForm2).serialize();
-			"email: "+ $("#submitForm2").val(),
-			function (data) {
-				data = $.parseJSON(data);
-				if (data["success"] == 1) {
-					// Good
-					$(".footer > .container").append("<p class=\"response good\">" + data["message"] + "</p>")
-				} else {
-					// Bad
-					$(".footer > .container").append("<p class=\"response bad\">" + data["message"] + "</p>")
-				}
-			}, 'json');
-	} 
+$("#submitForm2").submit(function () {
+	$.post(
+		"infuture.io/subscribe",
+		{email: $("#submitForm2 > input").val()},
+		function (data) {
+			data = $.parseJSON(data);
+			if (data["success"] == 1) {
+				// Good
+				$(".footer > .container").append("<p class=\"response good\">" + data["message"] + "</p>")
+			} else {
+				// Bad
+				$(".footer > .container").append("<p class=\"response bad\">" + data["message"] + "</p>")
+			}
+		}, 'json');
 });
 
-function validateEmail(sEmail) {
-	var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
-	if (filter.test(sEmail)) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
